43ab76ff5d8c5ad719ea913b64351e01f476630a
Bryce Slade <bryceslade@gmail.com>
2021-11-30 13:45:51 -0700

    GET post by idd is working

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -10,7 +10,25 @@ router.get('/', (req, res) => {
         })
         .catch(error => {
             res.status(500).json({
-                message: error.message
+                message: 'Error retrieving posts'
+            })
+        })
+})
+
+router.get('/:id', (req, res) => {
+    Posts.findById(req.params.id)
+        .then(post => {
+            if (post) {
+                res.status(200).json(post)
+            } else {
+                res.status(404).json({
+                    message: 'The post with the specified ID does not exist'
+                })
+            }
+        })
+        .catch(error => {
+            res.status(500).json({
+                message: 'Error retrieving post'
             })
         })
 })
